5d25cfa38c117d5eb2d86d96c933708c249ca0e5
newjay79 <161708358+newjay79@users.noreply.github.com>
2024-03-01 02:46:26 +0530

    Update utils.py

--- JayXSpamBot/utils.py
+++ JayXSpamBot/utils.py
@@ -13,7 +13,7 @@ def load_plugins(plugin_name):
     load = importlib.util.module_from_spec(spec)
     load.logger = logging.getLogger(plugin_name)
     spec.loader.exec_module(load)
-    sys.modules["JayXspamBot.plugins." + plugin_name] = load
+    sys.modules["JayXSpamBot.plugins." + plugin_name] = load
     print("[INFO] Successfully Imported" + plugin_name)
 
 async def edit_or_reply(event, text):
