15a6d8cb5f79984c7caff35ae52d40fb9b4a797a
newjay79 <161708358+newjay79@users.noreply.github.com>
2024-03-01 14:14:25 +0530

    Add files via upload

---
+++ config/config.py
@@ -0,0 +1,40 @@
+
+
+
+from os import getenv
+
+from dotenv import load_dotenv
+
+()
+
+API_ID = int(getenv("API_ID", ""))
+API_HASH = getenv("API_HASH")
+
+# ADMIN DETAILS (Your ID) 
+
+OWNER_ID = int(getenv("OWNER_ID", ""))
+
+OWNER_NAME = getenv("OWNER_NAME") 
+
+SUDO_USER= list(
+
+    map(int, getenv("SUDO_USER", "5937170640").split())
+
+)
+
+# BOT TOKEN CONFIG VARS (get all vars detail from @botfather) 
+
+n
+f("CMD_HNDLR") 
+
+LUND = list(
+
+    map(int, getenv("LUND", "123456789").split())
+
+)
+
+CHUT = list(
+
+    map(int, getenv("CHUT", "1234576789").split())
+
+)
