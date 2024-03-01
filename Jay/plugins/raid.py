f5eb8d1800d403aaf59d17eaabafe25a723ba19d
newjay79 <161708358+newjay79@users.noreply.github.com>
2024-03-01 11:26:00 +0530

    Update raid.py

--- Jay/plugins/raid.py
+++ Jay/plugins/raid.py
@@ -7,8 +7,8 @@ from telethon import events
 from telethon import functions, types
 from telethon.tl.functions.messages import ImportChatInviteRequest as Get
 from resources.data import RAID, REPLYRAID, DEADLYSPAM, BRTHSPAM
-from DollXSpamBot import BOT0, BOT1, BOT2, BOT3, BOT4, BOT5, BOT6, BOT7, BOT8, BOT9, SUDO_USERS, OWNER_ID
-from DollXSpamBot import CMD_HNDLR as hl
+from Jay import BOT0, BOT1, BOT2, BOT3, BOT4, BOT5, BOT6, BOT7, BOT8, BOT9, SUDO_USERS, OWNER_ID
+from Jay import CMD_HNDLR as hl
 
 
 que = {}
